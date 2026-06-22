39754a9abb3282fccb1c230e804eaa1c438326b5
Malene Wullink <malenewullink@gmail.com>
2026-06-22 17:27:44 +0200

    turtle crossing

---
+++ day23_turtle_crossing_game/scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 1
+        self.penup()
+        self.hideturtle()
+        self.goto(-280, 260)
+        self.update_score()
+
+    def add_score(self):
+        self.score+= 1
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Level: {self.score}", align="left", font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
