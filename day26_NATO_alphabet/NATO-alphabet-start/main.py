b9e362517e66dcab61b0a23d7dcdc642ce569b5c
Malene Wullink <malenewullink@gmail.com>
2026-06-29 15:27:46 +0200

    nato alphabet

---
+++ day26_NATO_alphabet/NATO-alphabet-start/main.py
@@ -0,0 +1,12 @@
+import pandas
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+dictionary = {}
+
+for (index, row) in data.iterrows():
+    dictionary[row.letter] = row.code
+
+user_input = input("Type a word to convert into NATO phonetic code: ").upper().strip()
+letters = list(user_input)
+nato_list = [dictionary[letter] for letter in letters]
+print(nato_list)
