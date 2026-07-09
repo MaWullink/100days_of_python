348cfc1ecced69cb06e29c3fd3e654c9e0fc5069
Malene Wullink <malenewullink@gmail.com>
2026-07-09 13:52:45 +0200

    add day 30 updated password manager

--- day26_NATO_alphabet/NATO-alphabet-start/main.py
+++ day26_NATO_alphabet/NATO-alphabet-start/main.py
@@ -6,7 +6,13 @@ dictionary = {}
 for (index, row) in data.iterrows():
     dictionary[row.letter] = row.code
 
-user_input = input("Type a word to convert into NATO phonetic code: ").upper().strip()
-letters = list(user_input)
-nato_list = [dictionary[letter] for letter in letters]
-print(nato_list)
+while True:
+    try:
+        user_input = input("Type a word to convert into NATO phonetic code: ").upper().strip()
+        letters = list(user_input)
+        nato_list = [dictionary[letter] for letter in letters]
+        print(nato_list)
+    except KeyError:
+        print("We only accept words.")
+
+
--- day29_password_manager/main.py
+++ day29_password_manager/main.py
@@ -2,6 +2,25 @@ from tkinter import *
 from tkinter import messagebox
 import secrets
 import string
+import json
+# ----------------------------SEARCH PASSWORD -------------------------------------#
+def search_password():
+    website = website_input.get()
+
+    try:
+        with open("passwords.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message=f"No file found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email} Password: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details found")
+
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -13,16 +32,30 @@ def save_password():
     website = website_input.get()
     email = email_input.get()
     password = password_input.get()
+    new_data = {website:{
+        "email": email,
+        "password": password
+    }}
 
     if len(website)==0 or len(email)==0 or len(password)==0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left anything blanc.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmal: {email}\nPassword: {password}\nIs it ok to save?")
-        if is_ok:
-            website_input.delete(0, END)
-            password_input.delete(0,END)
-            with open("passwords.txt", "a") as file:
-                file.write(f"{website} | {email} | {password}\n")
+            try:
+                with open("passwords.json", "r") as file:
+                    data = json.load(file)
+
+            except FileNotFoundError:
+                with open("passwords.json", "w") as file:
+                    json.dump(new_data, file, indent=2)
+            else:
+                data.update(new_data)
+                with open("passwords.json", "w") as file:
+                    json.dump(data, file, indent=2)
+            finally:
+                website_input.delete(0, END)
+                password_input.delete(0, END)
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -43,6 +76,7 @@ password_label = Label(text="Password:")
 # Buttons
 generate_password_btn = Button(text="Generate Password", command=generate_password)
 add_btn = Button(text="Add", width=36, command=save_password)
+search_btn = Button(text="Search",  command=search_password)
 
 # Input
 website_input = Entry(width=35)
@@ -53,14 +87,14 @@ password_input = Entry(width=21)
 # Layout
 canvas.grid(row=0, column=1)
 website_label.grid(row=1, column=0)
-website_input.grid(row=1, column=1, columnspan=2, sticky="ew")
+website_input.grid(row=1, column=1, columnspan=1, sticky="ew")
 email_label.grid(row=2, column=0)
 email_input.grid(row=2, column=1, columnspan=2, sticky="ew")
 password_label.grid(row=3, column=0)
 password_input.grid(row=3, column=1, sticky="ew")
 generate_password_btn.grid(row=3, column=2)
 add_btn.grid(row=4, column=1, columnspan=2, sticky="ew")
-
+search_btn.grid(row=1, column=2, sticky="ew")
 
 
 
